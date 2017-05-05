f859d33d9925d36298e977951845ff4826805225
caul1696 <u201315179@upc.edu.pe>
2017-05-04 22:41:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,52 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
-</body>
-</html>
+ <html>
+ <body>
+ <div name="top" align="center">
+ 	<img src="images/top.jpg" width="1500" height="150"/>
+ </div>
+ <div name="middle" align="center">
+ <table>
+	  	<?php
+		$server="us-cdbr-iron-east-03.cleardb.net";
+		$user="b09646d04a4850";
+		$pass="18a68973";
+		$db="ad_091359118c42411";
+		$conn=new mysqli($server, $user, $pass, $db);
+		if($conn->connect_error){
+			die("Connection Failed: ".$conn->connect_error);
+		}
+		$sql="select * from servicios";
+		$result=$conn->query($sql);
+		if($result->num_rows > 0){
+			?>
+			<tr>
+				<th>Codigo</th>
+				<th>Nombre</th>
+				<th>Descripcion</th>
+				<th>Imagen</th>
+			</tr>
+			<?php
+			while($row=$result->fetch_assoc()){
+				?>
+				<tr>
+					<td><?php echo $row['idAlumno']?></td>
+					<td><?php echo $row['nombre']?></td>
+					<td><?php echo $row['edad']?></td>
+					<td><img src="<?php echo $row['imagen_servicio']?>"/></td>
+				</tr>
+				<?php
+			}
+		}
+		else{
+			echo "0 result";
+		}
+		$conn->close();
+		?>
+	  </table>
+	 </div>
+ <div name="bottom" align="center">
+ 	<img src="images/top.jpg" width="1500" height="150"/>
+ </div>
+  
+ </body>
+ </html>
